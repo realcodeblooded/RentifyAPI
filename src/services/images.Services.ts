5913eef19bf326e0301a0bf76c3331c85f5a4e6a
kennynet66 <kennynet66@gmail.com>
2026-02-11 18:26:39 +0300

    Add building validations

--- src/services/images.Services.ts
+++ src/services/images.Services.ts
@@ -1,11 +1,16 @@
 import multer from "multer";
 
+/**
+ * @constant ALLOWED_MIME_TYPES
+ * * Array of allowed file types
+ * 
+ * @constant ALLOWED_FILE_SIZE
+ * Allowed file size
+ */
+export const ALLOWED_MIME_TYPES: string[] = ["image/png", "image/jpeg", "image/jpg"];
+export const ALLOWED_FILE_SIZE: number = 1 * 1024 * 1024; // 1MB
+
 export const IMAGES_TO_BASE64 = (IMAGES: Express.Multer.File[], FILE_NUMBER_LIMIT: number): { Error?: string | null, Images: string[] } => {
-    /**
-     * @constant ALLOWED_MIME_TYPES
-     * * Array of allowed file types
-     */
-    const ALLOWED_MIME_TYPES: string[] = ["image/png", "image/jpeg", "image/jpg"];
 
     // Ensure at least one image is given
     if (IMAGES.length < 1) {
@@ -33,7 +38,7 @@ export const IMAGES_TO_BASE64 = (IMAGES: Express.Multer.File[], FILE_NUMBER_LIMI
         }
 
         // Validate image size
-        const IMAGE_SIZE_LIMIT = image.size / (1 * 1024 * 1024);
+        const IMAGE_SIZE_LIMIT = image.size / ALLOWED_FILE_SIZE;
         if (IMAGE_SIZE_LIMIT > 1) {
             return {
                 Error: `${image.originalname} file size exceeds 1 MB limit`,
